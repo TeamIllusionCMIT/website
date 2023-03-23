b273aeaec464f8c4c67e55959e36e4c777c31789
thrzl <thrizzle@skiff.com>
2023-03-23 13:03:52 +0000

    ✨ add logo to top right

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -3,7 +3,7 @@
     let _text = text.repeat(500)
 </script>
 
-<div class="max-h-120vh w-screen overflow-clip absolute -z-100">
+<div class="h-screen w-screen overflow-clip absolute -z-100">
     <div class="m-0 h-full w-[105vw] bg-black text-neutral-800 m-0 break-words" style="font-family: Array-Wide; font-size: 3rem; line-height:2.5rem">
         {_text}
     </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,7 @@
     import "../app.css";
     import "uno.css"
 </script>
+<div class="fixed top-15 right-15">
+    <img src="./i-horiz.webp" alt="Team Illusion Logo. An I with a star swirling around it." width={64} height={64} class="ml-5">
+</div>
 <slot/>
