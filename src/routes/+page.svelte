921bbf0bfb2bb09a945efefa56ee2646946e0c45
thrzl <terry.bazemoreiii@gmail.com>
2023-04-23 20:42:06 +0000

    ✨ add about page image + add contact form n hcb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     let links = [
         { href: "/about", text: "About" },
         { href: "/contact", text: "Contact" },
-        { href: "/stats", text: "Stats" },
+        { href: "https://bank.hackclub.com/teamillusion", text: "Finances"}
     ];
 </script>
 <Background/>
@@ -11,8 +11,8 @@
 <div class="h-screen w-screen" style="font-family: Inter">
     <header class="mx-auto flex flex-col items-center justify-center text-white lg:max-w-screen-xl w-screen h-screen m-auto">
         <div class="text-left w-2/3">
-            <p class="text-neutral-400 hover:text-white duration-50 text-2xl my-0 font-800 -spacing-1">TEAM ILLUSION</p>
-            <p class="text-7xl my-0 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 font-800 -spacing-3">CMIT's official VEX and FIRST robotics team.</p>
+            <p class="text-neutral-400 hover:text-white duration-250 text-2xl my-0 font-800 -spacing-1">TEAM ILLUSION</p>
+            <p class="text-7xl my-0 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 font-800 -spacing-3">Chesapeake Math & IT North's official VEX and FIRST robotics team.</p>
             <div class="rise-wrapper whitespace-pre w-fit font-800 -spacing-1">
                 {#each links as {href, text}, index}
                     <a href={href} class="rise-anim md:inline-flex block text-neutral-400 text-xl mr-5 decoration-none hover:text-white duration-250" style={`--delay: ${index * 200 + 100}ms`}
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<iframe
+	src="https://forms.fillout.com/t/eBEmMDDsadus"
+	width="100%"
+	height="100%"
+	frameborder="0"
+	marginheight="0"
+	marginwidth="0"
+	title="Team Illusion Contact Form"
+    class="h-screen w-screen"
+/>
