d2da4b8291fd3f437da151283a0722a80a71fb7a
thrzl <terry.bazemoreiii@gmail.com>
2023-04-28 00:10:59 +0000

    📱 fix mobile contents extending past page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,12 +10,12 @@
 
 <div class="h-screen w-screen" style="font-family: Inter">
     <header class="mx-auto flex flex-col items-center justify-center text-white lg:max-w-screen-xl w-screen h-screen m-auto">
-        <div class="text-left w-2/3">
+        <div class="text-left w-3/4 md:w-2/3">
             <p class="text-neutral-400 hover:text-white duration-500 text-2xl my-0 font-800 -spacing-1 ease-out">TEAM ILLUSION</p>
-            <p class="text-7xl my-0 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 font-800 -spacing-3">Chesapeake Math & IT North's official VEX and FIRST robotics team.</p>
-            <div class="rise-wrapper whitespace-pre w-fit font-800 -spacing-1">
+            <p class="text-5xl md:text-7xl my-0 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 font-800 -spacing-3"><span class="hidden md:inline">Chesapeake Math & IT North's</span> <span class="md:hidden inherit">CMIT North's</span> official VEX and FIRST robotics team.</p>
+            <div class="rise-wrapper whitespace-pre w-fit max-w-4/5 font-800 -spacing-1 flex flex-wrap flex-row">
                 {#each links as {href, text}, index}
-                    <a href={href} class="rise-anim md:inline-flex block text-neutral-400 text-xl mr-5 decoration-none hover:text-white duration-500 ease-out" style={`--delay: ${index * 200 + 100}ms`}
+                    <a href={href} class="rise-anim text-neutral-400 text-xl mr-5 decoration-none hover:text-white duration-500 ease-out w-fit" style={`--delay: ${index * 200 + 100}ms`}
                         >{`${text}`}</a
                     >
 				{/each}
--- src/routes/finances/+page.svelte
+++ src/routes/finances/+page.svelte
@@ -7,23 +7,23 @@
     <header class="mx-auto flex flex-col items-center justify-center text-white lg:max-w-screen-xl w-screen h-screen m-auto">
         <div class="text-left w-2/3">
             <p class="text-neutral-400 hover:text-white duration-500 text-2xl my-0 font-800 -spacing-1 ease-out">TEAM ILLUSION</p>
-            <p class="text-7xl my-0 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 font-800 -spacing-3">Finances</p>
+            <p class="text-5xl md:text-7xl my-0 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 font-800 -spacing-3">Finances</p>
         </div>
-        <div class="w-2/3">
+        <div class="max-w-3/4 w-4xl overflow-x-auto">
             <table class="border-collapse border border-slate-500 w-full">
             <thead>
                 <tr>
-                  <th class="text-left text-3xl -spacing-1 p-3 border-1 b-t-0 b-l-0 border-solid border-black border-neutral-600">Date</th>
-                  <th class="text-left text-3xl -spacing-1 p-3 border-1 b-t-0 border-solid border-black border-neutral-600">Memo</th>
-                  <th class="text-left text-3xl -spacing-1 p-3 border-1 b-t-0 b-r-0 border-solid border-black border-neutral-600">Amount</th>
+                  <th class="text-left text-xl md:text-3xl -spacing-1 p-3 border-1 b-t-0 b-l-0 border-solid border-black border-neutral-600">Date</th>
+                  <th class="text-left text-xl md:text-3xl -spacing-1 p-3 border-1 b-t-0 border-solid border-black border-neutral-600">Memo</th>
+                  <th class="text-left text-xl md:text-3xl -spacing-1 p-3 border-1 b-t-0 b-r-0 border-solid border-black border-neutral-600">Amount</th>
                 </tr>
               </thead>
               <tbody>
                 {#each data.transaction as transaction}
                   <tr class={`w-full ${transaction.amount_cents < 0 ? 'bg-[#110000]' : 'bg-[#001100]'}`}>
-                    <td class="border-1 -spacing-1 font-bold border-solid border-black border-neutral-600 p-4">{transaction.date}</td>
-                    <td class="border-1 -spacing-1 font-bold border-solid border-black border-neutral-600 p-4">{transaction.memo}</td>
-                    <td class="border-1 -spacing-1 font-bold border-solid border-black border-neutral-600 p-4">{transaction.amount_cents < 0 ? "-": ""}${Math.abs(transaction.amount_cents/100)}</td>
+                    <td class="border-1 -spacing-1 text-sm md:text-md font-bold border-solid border-black border-neutral-600 p-4">{transaction.date}</td>
+                    <td class="border-1 -spacing-1 text-sm md:text-md font-bold border-solid border-black border-neutral-600 p-4">{transaction.memo}</td>
+                    <td class="border-1 -spacing-1 text-sm md:text-md font-bold border-solid border-black border-neutral-600 p-4">{transaction.amount_cents < 0 ? "-": ""}${Math.abs(transaction.amount_cents/100)}</td>
                   </tr>
                 {/each}
               </tbody>
