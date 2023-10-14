738a5533ac1597e740ee7b22652bc81e3afdb165
te <thrzl@icloud.com>
2023-10-13 21:07:22 -0400

    💄 ensure the home button stays on top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     import "uno.css"
 </script>
 <a href="/">
-    <div class="fixed top-15 right-15">
+    <div class="fixed top-15 right-15 z-999">
         <img src="./i-horiz.webp" alt="Team Illusion Logo. An I with a star swirling around it." width={64} height={64} class="ml-5 scale-100 hover:scale-120 transition-500 ease-out">
     </div>
 </a>
