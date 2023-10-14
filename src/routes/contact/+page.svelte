a3370eeab67746d9a9306367cd7ecd135b7fc16b
te <thrzl@icloud.com>
2023-10-13 21:07:48 -0400

    ⚡️ use tally instead of fillout

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,11 +1,34 @@
-<div class="w-screen h-screen bg-black"><iframe
-	src="https://forms.fillout.com/t/eBEmMDDsadus"
-	width="100%"
-	height="100%"
-	frameborder="0"
-	marginheight="0"
-	marginwidth="0"
-	title="Team Illusion Contact Form"
-    class="h-screen w-screen"
-/>
+<div class="h-screen w-screen" style="font-family: Inter">
+	<header
+		class="mx-auto flex flex-col items-center justify-center text-white lg:max-w-screen-xl w-screen h-screen m-auto"
+	>
+		<div class="w-2/3">
+			<iframe
+				data-tally-src="https://tally.so/embed/n9ZJbV?alignLeft=1&transparentBackground=1&dynamicHeight=1"
+				loading="lazy"
+				width="100%"
+				height="476"
+				frameborder="0"
+				marginheight="0"
+				marginwidth="0"
+				title="Team Illusion Contact Form
+				"
+			></iframe><script>
+				var d = document,
+					w = 'https://tally.so/widgets/embed.js',
+					v = function () {
+						'undefined' != typeof Tally
+							? Tally.loadEmbeds()
+							: d.querySelectorAll('iframe[data-tally-src]:not([src])').forEach(function (e) {
+									e.src = e.dataset.tallySrc;
+							  });
+					};
+				if ('undefined' != typeof Tally) v();
+				else if (d.querySelector('script[src="' + w + '"]') == null) {
+					var s = d.createElement('script');
+					(s.src = w), (s.onload = v), (s.onerror = v), d.body.appendChild(s);
+				}
+			</script>
+		</div>
+	</header>
 </div>
