4268162aa75d2e4c6b58d0269f85e74d3c105c4e
te <thrzl@icloud.com>
2024-04-28 16:33:59 -0400

    ✨ add contact form to homepage

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,12 +1,13 @@
 <script>
-export const text = "TEAMILLUSION";
-export const first = false;
+export let text = "TEAMILLUSION";
+export let first = false;
+export let last = false;
 const _text = text.repeat(500);
 </script>
 
 
 <div class="h-screen w-screen overflow-clip absolute -z-100 ">
-    <div class="absolute left-0 bottom-0 w-screen h-40 bg-gradient-to-b opacity-85 from-transparent to-black z-10 mb-0 p-0"></div>
+    {#if !last}<div class="absolute left-0 bottom-0 w-screen h-40 bg-gradient-to-b opacity-85 from-transparent to-black z-10 mb-0 p-0"></div>{/if}
     {#if !first}<div class="absolute left-0 top-0 w-screen h-20 bg-gradient-to-t opacity-85 from-transparent to-black z-10 mb-0 p-0"></div>{/if}
     <div class="m-0 h-full w-[105vw] bg-black text-neutral-900 m-0 break-words" style="font-family: Array-Wide; font-size: 3rem; line-height:2.5rem">
         {_text}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,12 @@ import links from "../data/links.json";
 import sponsors from "../data/sponsors.json";
 </script>
 
-<Background text="TEAMILLUSION"/>
+<style>
+	button {
+		background-color: #6000FF;
+	}
+</style>
+<Background text="TEAMILLUSION" first={true}/>
 <div class="section-wrapper">
 	<section class="section">
 		<div class="text-left w-3/4 md:w-2/3">
@@ -63,7 +68,7 @@ import sponsors from "../data/sponsors.json";
 			</p>
 		</div>
 		<div
-			class="bg-neutral-600 bg-opacity-10 backdrop-blur-md w-screen md:max-w-4/5 p-10 my-10 rounded-xl text-center"
+			class="bg-[neutral-600] bg-opacity-20 backdrop-blur-md w-screen md:max-w-4/5 p-10 my-10 rounded-xl text-center"
 		>
 			<div class="md:flex justify-center flex-wrap flex-gap-10 hidden">
 				{#each sponsors as { logo, url, name }}
@@ -114,9 +119,61 @@ import sponsors from "../data/sponsors.json";
             <a href="about" style="letter-spacing: -0.5px" class="text-2xl button"><img src="message.svg" alt="paperclip denoting an attachment" class="h-[2em] text-xl" />Learn more</a>
 		</div>
 	</section>
-	<div class="flex absolute bottom-10 w-screen items-center justify-center">
+</div>
+<hr class="border-neutral-600 mx-auto opacity-40 w-1/4" />
+<Background text="CONTACT" last={true}/>
+<!-- a form that asks for a name, email, message, and has a dropdown for purpose of contact -->
+<div class="section-wrapper relative">
+	<section class="section">
+		<div class="text-left w-3/4 md:w-2/3">
+			<h1
+				class="text-white opacity-60 hover:opacity-100 duration-500 text-2xl my-0 font-800 -spacing-1 ease-out"
+			>
+				CONTACT
+			</h1>
+			<h2
+				class="text-5xl md:text-7xl my-0 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 font-800 -spacing-3 pb-2"
+			>
+				Let's talk.
+			</h2>
+
+			<form
+				name="contact"
+				netlify
+				class="flex flex-col flex-gap-2 mt-2 font-[Inter] font-bold -spacing-1 text-lg text-neutral-500"
+			>
+				<input
+					type="text"
+					name="name"
+					placeholder="Name"
+					class="bg-neutral-800 rounded-lg text-md py-2 pl-4 b-neutral-700 b-none font-[Inter] font-bold -spacing-1 text-lg text-white"
+				/>
+				<input
+					type="email"
+					name="email"
+					placeholder="Email"
+					class="bg-neutral-800 rounded-lg text-md py-2 pl-4 b-neutral-700 b-none font-[Inter] font-bold -spacing-1 text-lg text-white"
+				/>
+				<textarea
+					name="message"
+					placeholder="Message"
+					class="bg-neutral-800 rounded-lg text-md py-2 pl-4 b-neutral-700 b-none font-[Inter] font-bold -spacing-1 text-lg text-white"
+				></textarea>
+				<select
+					name="purpose"
+					class="bg-neutral-800 rounded-lg text-md py-2 pl-4 font-bold -spacing-1 text-lg text-white"
+				>
+					<!-- <option value="" disabled selected class="text-neutral-500">Contact Reason</option> -->
+					<option value="general" class="text-white">General Inquiry</option>
+					<option value="sponsorship">Sponsorships</option>
+					<option value="other">Other</option>
+				</select>
+				<button type="submit" class="button mt-0 rounded-lg">Submit</button>
+			</form>
+		</div>
+	</section>
+	<div class="flex absolute bottom-5 md:bottom-10 w-screen items-center justify-center">
 		<span class="font-spacemono text-white opacity-50 px-5 text-center hover:opacity-100 duration-500 ease-out"
-			>Fiscally sponsored by <a href="https://hackclub.com/fiscal-sponsorship" class="text-[#ec3750] b-[#ec3750] b-b-1 hover:b-b-2">The Hack Foundation</a>. Nonprofit EIN: 81-2908499</span
 		>
 	</div>
 </div>
