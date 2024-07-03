6b9f29eff02f2a323237411e08774a77cd5add39
te <thrzl@icloud.com>
2024-07-03 18:07:10 -0400

    💫 add page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script>
 import "virtual:uno.css";
 import "../app.css";
+export let data;
+
+data.pathname;
+
+import { fade } from 'svelte/transition';
 </script>
 <a href="/">
     <div class="fixed top-5 right-5 md:top-15 md:right-15 z-999">
         <img src="./i-horiz.webp" alt="Team Illusion Logo. An I with a star swirling around it." width={64} height={64} class="w-15 h-15 md:w-20 md:h-20 ml-5 scale-100 hover:scale-120 transition-500 ease-out">
     </div>
 </a>
-<slot/>
+{#key data.pathname}
+<div in:fade={{ duration: 300, delay: 400 }} out:fade={{ duration: 300 }}>
+    <slot/>
+</div>
+{/key}
